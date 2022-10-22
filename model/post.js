b7dd58d3c2530702ba1cfe4e57f87b8fbe7bf94b
bui huy bach <bachkame@gmail.com>
2022-10-22 22:33:07 +0700

    them gui cv

--- model/post.js
+++ model/post.js
@@ -5,13 +5,16 @@ const PostsSchema = new Schema({
   workName: { type: String, default: "", require: true },
   workAddress: { type: String, default: "", require: true },
   deadline: { type: String, require: true },
-  salary: { type: Number, require: true },
+  salary: { type: String, require: true },
   hinhThucLamViec: { type: String, require: true },
   sex: { type: String, require: true },
   amount: { type: Number, require: true },
   level: { type: String, require: true },
   exp: { type: String, require: true },
   detailInfo: { type: String, require: true },
+  creatorId: { type: String, require: true },
+  isConfirm: { type: Boolean, default: false },
+  Cv: {type: Array, default: []}
 });
 
 module.exports = mongoose.model("post", PostsSchema);
--- routes/post.js
+++ routes/post.js
@@ -7,22 +7,18 @@ Router.post("/create", async (req, res) => {
 
   try {
     if (!data) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: "Tạo bài tuyển dụng không thành công",
-        });
+      return res.status(400).json({
+        success: false,
+        message: "Tạo bài tuyển dụng không thành công",
+      });
     }
     const newPost = new PostSchema(data);
     await newPost.save();
-    return res
-      .status(200)
-      .json({
-        success: true,
-        message: "Tạo bài tuyển dụng thành công",
-        post: newPost,
-      });
+    return res.status(200).json({
+      success: true,
+      message: "Tạo bài tuyển dụng thành công",
+      post: newPost,
+    });
   } catch (err) {
     console.log(err);
     return res
@@ -39,12 +35,10 @@ Router.post("/get-post", async (req, res) => {
   try {
     const post = await PostSchema.findById(postId);
     if (!post) {
-      return res
-        .status(400)
-        .json({
-          success: true,
-          message: "Không tìm thấy bài đăng tuyển dụng này",
-        });
+      return res.status(400).json({
+        success: true,
+        message: "Không tìm thấy bài đăng tuyển dụng này",
+      });
     }
 
     return res.status(200).json({ success: true, post: post });
@@ -53,4 +47,37 @@ Router.post("/get-post", async (req, res) => {
   }
 });
 
+//lấy các post chưa được duyệt
+Router.post("/get-my-post", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const _res = await PostSchema.find({ creatorId: userId });
+
+    return res.status(200).json({ success: true, posts: _res });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ success: false, error: err });
+  }
+});
+
+// cập nhật CV
+
+Router.post("/add-cv", async (req, res) => {
+  const Cv_Url = req.body.Cv_Url;
+  const postId = req.body.postId;
+  const senderId = req.body.senderId;
+  try {
+    const _res = await PostSchema.findByIdAndUpdate(postId, {
+      $push: { Cv: { sender: senderId, url: Cv_Url } },
+    });
+
+    res.status(200).json({ success: true, message: "Gửi CV thành công!" });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(400)
+      .json({ success: true, message: "Gửi CV không thành công!" });
+  }
+});
 module.exports = Router;
